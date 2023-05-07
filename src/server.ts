5fae64a955f829a3dc2fdd99d62707e72d9f71f3
Joaoof <joaodeus400@gmail.com>
2023-05-07 16:07:52 -0300

    rotas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,22 @@ import express from 'express'
 
 const app = express()
 
+app.get('users', (request, response) => {
+  return response.json(['usuário 1'])
+})
+
+app.post('./users', (request, response) => {
+  return response.json({ message: 'Criando usuário' })
+})
+
+app.put('./users', (request, response) => {
+  return response.json({ message: 'Atualizando usuário' })
+})
+
+app.delete('./users', (request, response) => {
+  return response.json({ message: 'Deletando usuário '})
+})
+
 app.listen('3333', () => {
   console.log('BAck-end started!')
 })
