363f8fb9f87a1c9961dacb01a9bbf4722b48808a
Joaoof <joaodeus400@gmail.com>
2023-05-10 01:30:08 -0300

    rotas

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { v4 as uuid } from 'uuid'
 
 const app = express()
 
@@ -13,19 +14,47 @@ interface User {
 const users: User[] = []
 
 app.get('/users', (request, response) => {
-  return response.json(['usuário 1'])
+  return response.json(users)
 })
 
 app.post('/users', (request, response) => {
-  return response.json({ message: 'Criando usuário' })
+  const { name, email } = request.body
+
+  const user = { id: uuid(), name, email }
+
+  users.push(user)
+
+  return response.json(user)
 })
 
-app.put('/users', (request, response) => {
-  return response.json({ message: 'Atualizando usuário' })
+app.put('/users/:id', (request, response) => {
+  const { id } = request.params
+  const { name, email } = request.body
+
+  const userIndex = users.findIndex((user) => user.id == id)
+
+  if (userIndex < 0 ) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  const user = { id, name, email }
+  users[userIndex] = user
+
+  return response.json(user)
 })
 
-app.delete('/users', (request, response) => {
-  return response.json({ message: 'Deletando usuário '})
+app.delete('/users/:id', (request, response) => {
+  const { id } = request.params
+
+  const userIndex = users.findIndex((user) => user.id == id)
+
+  if (userIndex < 0 ) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  users.splice(userIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen('3333', () => {
